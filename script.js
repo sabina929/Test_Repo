e30e7c577a019db14c85d1db5a8e8ff85cc6006d
sabina929 <sabina.abbasova2909@gmail.com>
2019-08-22 19:28:48 +0400

    test project

---
+++ script.js
@@ -0,0 +1,14 @@
+const ipsumAnimi = document.querySelector('.ipsum-animi');
+const paragraphs = document.querySelectorAll('.parag');
+// console.log(paragraphs);
+
+ipsumAnimi.addEventListener('click', animateParagraphs);
+
+
+function animateParagraphs() {
+    paragraphs.forEach(parag => {
+        console.log(parag);
+        parag.classList.toggle('justifying');
+    })
+
+}
